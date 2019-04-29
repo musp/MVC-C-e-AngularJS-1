b90f2a182f09d86fc2de27701c1683e757838875
Mauricio Pacheco <musphard@gmail.com>
2019-04-29 16:00:29 -0300

    Limpeza dos códigos, gerados automaticamente pelo visual studio.

---
+++ AngularComMVC/AngularComMVC/Controllers/InicioController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace AngularComMVC.Controllers
+{
+    public class InicioController : Controller
+    {
+        // GET: Inicio
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
